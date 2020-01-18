7b0acc059b2f1f1b3c29f214ac535b66ed9e451b
Виталий <chernickovvit@yandex.ru>
2020-01-18 14:27:34 +0300

    --version=true=3

--- src/main/java/com/cko/sampleSpringProject/controller/MainController.java
+++ src/main/java/com/cko/sampleSpringProject/controller/MainController.java
@@ -12,18 +12,50 @@ public class MainController {
     MailService mailService;
 
     @GetMapping("/login")
-    public String showLoginPage(){
+    public String showLoginPage() {
         return "LoginPage";
     }
+
     @GetMapping("/")
-    public String showMainPage(){
+    public String showMainPage() {
         return "MainPage";
     }
+
     @GetMapping("/test")
-    public String sendTestMessage(){
+    public String sendTestMessage() {
         String first = "kosmynin.lenya@yandex.ru";
         String second = "chernickovvit.public@gmail.com";
-        mailService.sendSimpleMessage(first,"@TestMessage","Simple message...");
+        mailService.sendSimpleMessage(first, "@TestMessage", "Simple message...");
         return "MainPage";
+    }
+
+    @GetMapping("/post")
+    public String post() {
+        return "index";
+    }
+
+    @GetMapping("/sent")
+    public String sent() {
+        return "sent";
+    }
+
+    @GetMapping("/basket")
+    public String basket() {
+        return "basket";
+    }
+
+    @GetMapping("/favorites")
+    public String favorites() {
+        return "favorites";
+    }
+
+    @GetMapping("/_login")
+    public String LoginOther() {
+        return "OtherLogin";
+    }
+
+    @GetMapping("/write")
+    public String write() {
+        return "write";
     }
 }
